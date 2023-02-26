ee59657ab48b7de483a6220a04e041075a43e6a0
sangminLee <sm0514sm@gmail.com>
2023-02-26 14:52:36 +0900

    Update CreatePostService.py

--- service/CreatePostService.py
+++ service/CreatePostService.py
@@ -22,7 +22,7 @@ class CreatePostService:
            f"- Content goal: blog" \
            f"- The first line should contain the title." \
            f"- The title is formatted as follows TITLE: write title here" \
-           f"- Then use subheadings to categorize and explain.
+           f"- Then use subheadings to categorize and explain." \
            f'- Use the Unsplash API like this: <img src="https://source.unsplash.com/1600x900/?" alt="?">' \
            f'- ( After the "?", you can put a representative word from the sentence. ) ' \
            f'- The second line should contain <img>.' \
